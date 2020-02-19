e571ff052bed3472817f0d653a678a21ab059011
VPozdnyakov <systemoperation@mail.ru>
2020-02-19 22:41:06 +0300

    add div and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  return(
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://image.useinsider.com/sportmaster/defaultImageLibrary/Pop_up_card-6CXlzOs9Zysj7O4x5sUi1560864030.png'/>
       </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='http://cdn.sptmr.ru/upload/dip_content/2020/hp/8-week/images/slide3.jpg'/>
+        </div>
+        <div>
+          ava+ descr
+        </div>
+        <div>
+          ava+ descr
+        </div>
+          <div>
+            My postr
+            <div>
+              New Post
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
